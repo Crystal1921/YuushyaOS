2eb58c36db8c13aded9eacc603e059fe8568bc0b
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2026-03-25 11:07:58 +0800

    初始化

---
+++ src/main/java/com/yuushya/yuushya_os/YuushyaOS.java
@@ -0,0 +1,14 @@
+package com.yuushya.yuushya_os;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(YuushyaOS.MODID)
+public class YuushyaOS {
+    public static final String MODID = "yuushya_os";
+
+    public YuushyaOS(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
